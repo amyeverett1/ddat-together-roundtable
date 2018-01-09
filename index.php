370e04de7404edbbfa0533a85c58d7307305050d
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-01-09 12:18:28 +0000

    changed bedfont to 28th feb

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                 
               <ul>
                 <li>Hendon - 7 February</li>
-                <li>Bedfont Lakes - 27 February</li>
+                <li>Bedfont Lakes - 28 February</li>
                <!-- <li>Manchester - 7 March</li>
                 <li>Liverpool - 8 March</li>
                 <li>Southport - 8 March</li>
