4e255a92039fb644d5eed882bbcafa122ccbe7ef
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-04-16 22:01:09 +0100

    croydon page

--- croydon.php
+++ croydon.php
@@ -3,15 +3,15 @@
 </header>
 
 <div class="date">
-    <span>TBC April 2018 <br> Metropoint, Croydon</span>
+    <span>13:30-15:00, 9 May 2018<br>4th floor Metropoint, Croydon</span>
 </div>
 
-<div class="container index"> 
-<div class="accordion">    
-    
-    
+<div class="container index">
+<div class="accordion">
+
+
      <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Agenda</span></h3>
-    
+
 <div>
 <div class="wrapper">
          <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team. They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT.</p>
@@ -20,77 +20,78 @@
 
             <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events. This is your roundtable to find out more about the wider work of DDaT and discuss developments, local issues and concerns. It is also an opportunity to share information about your work, what your team is doing and your successes.  You can also raise questions and issues in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions or queries using this link</a>.</p>
 
+<p>The event will be hosted by Joanna Davinson, Steve Connell and Jackie Keane.</p>
             <p>These events are coordinated by <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications</a>.</p>
-            <p><strong>TBC Start</strong></p>     
+            <p><strong>13:30 Start</strong></p>
             <ul>
-                <li>Welcome and open – Pete Rose</li>
+                <li>Welcome and open – Joanna</li>
                 <li>Introductions – All</li>
-                <li>Business updates and key developments – Pete Rose</li>
+                <li>Business updates and key developments – Joanna</li>
                 <li>Hendon updates including successes – All</li>
                 <li>Questions and discussion topics submitted in advance by attendees – All</li>
              </ul>
-            <p><strong>TBC Close</strong></p>
-            <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p> 
+            <p><strong>15:00 Close</strong></p>
+            <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p>
     </div> <!-- wrapper-->
-    
+
   </div><!-- end agenda-->
-    
-    
-    
+
+
+
     <h3><i class="fa fa-user-plus fa-lg" aria-hidden="true"></i> <span>Register to attend</span></h3>
   <div>
 
 <div class="wrapper white">
     <p>Tickets coming soon.</p>
     <p class="big-space">Places will be available on a first come, first served basis.  If we reach the maximum number for the space available names will be added to a reserve list.  If you need to cancel your place, eg for operational reasons, please email the <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications inbox</a> as soon as possible so we can reallocate your place. </p>
-     </div><!--wrapper--> 
+     </div><!--wrapper-->
 
   </div><!-- end register-->
 
-   
-    
-    
-    
+
+
+
+
     <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Venue</span></h3>
   <div>
-   
-   <div class="wrapper">    
+
+   <div class="wrapper">
     <div class="box white">
-    
+
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2490.334079933692!2d-0.0988930492907266!3d51.37853772783875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876072d80c6d895%3A0xdd988862d942287c!2sMetro+Point!5e0!3m2!1sen!2suk!4v1514646061689" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div><!--boxwhite--> 
-</div><!--wrapper--> 
-      
-      
+    </div><!--boxwhite-->
+</div><!--wrapper-->
+
+
 <div class="wrapper">
      <p>Don’t forget to bring your Home Office pass. You must show it when you arrive. If you don’t bring it, you may not be able to gain access to the building.</p>
-    
+
     <h3>Address</h3>
    <p>Metropoint<br>
 49 Sydenham Road<br>
        Croydon<br>
        CR0 2EU</p>
-    
-     </div><!--wrapper--> 
+
+     </div><!--wrapper-->
 
   </div><!-- end venue-->
 
- 
-   
 
 
-  
-    
-    
+
+
+
+
+
 <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i>  <span>Submit your questions</span></h3>
   <div>
 
 <div class="wrapper">
     <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events.</p>
-    
+
 <p>We are keen for attendees to raise questions, issues and suggested discussion topics in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please click on the following link: <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/</a> .</p>
-  
-     </div><!--wrapper--> 
+
+     </div><!--wrapper-->
 
 
   </div><!-- end info-->
@@ -106,14 +107,14 @@
   <p>'Let’s Talk About...' is a team briefing tool which is distributed to grades SEO and above for discussing with their teams. Each month we will highlight a specific topic for you to discuss. The topic will be an issue which is important to Capabilities &amp; Resources (C&amp;R) and on which we would like you to feed back your team’s thoughts.</p>
 
     <p>'Let’s Talk About...' gives you and your team an opportunity to get involved in how we shape our business, share good practice and make your voice heard. Each issue of 'Let’s Talk About...' will be sponsored by a leader in C&amp;R. This issue is sponsored by James Martin from Home Office Cyber Security Centre and is about Cyber Security.</p>
- 
+
     <h3>This Month</h3>
 <p>The vast majority of us spend at least part of each working day in front of a computer. Technology is continuing to revolutionise the way we live our lives and carry out our jobs; however, these rapid developments have been matched by the spread of increasingly sophisticated cyber crime. As the recent attack on the NHS and other institutions demonstrated, the UK’s infrastructure is vulnerable to cyber crime and the consequences of a security breach can be costly and potentially devastating.</p>
-    
+
 <p>The nature of cyber security means that we all share responsibility for the protection of the Home Office’s IT system, not just the IT security team. We all play a vital role in keeping our systems secure; one mistake is all it can take for the defences to be breached and an attack to be successful.</p>
-    
+
 <p>In this month’s 'Let’s Talk About...', which is sponsored by James Martin from the Home Office Cyber Security Centre, we’ll be focusing on how you can keep Home Office systems and data safe.</p>
-    
+
 <strong>Keeping Home Office data and systems safe</strong>
 <p>The Home Office receives 170,000 spam emails a month and a proportion of these messages are hitting staff inboxes.</p>
 <p>Senders of these spam messages tried to masquerade themselves as Home Office staff by using a Home Office user’s name and email address to visually trick the recipient into reading the email and clicking on attached documents and links.</p>
@@ -121,28 +122,28 @@
 <p>Finally, refrain from using your Home Office email address for external logins and mailing lists. These details are sold to criminals and provide fresh opportunities to launch cyber attacks.</p>
 <strong>Remember – one click could result in our information being stolen, cost money, damage our reputation or stop us from doing our work.</strong>
 
-    
+
     <h3>Suggested Questions</h3>
 
     <p>Please take time to discuss the following questions at your next team meeting:</p>
 
 <ul>
-    <li>Q: When someone mentions ‘cyber security’, what do you think of?</li> 
+    <li>Q: When someone mentions ‘cyber security’, what do you think of?</li>
 <li>Q: What do you think your role should be in protecting the department from cyber security threats?</li>
 <li>Q:  What information would you like to see on cyber security? </li>
     </ul>
-    
-    
+
+
     <h3>Feedback</h3>
 <p>A summary of the feedback received on this topic will be shared as part of the next 'Let’s Talk About...' in July.</p>
 
- 
-     </div><!--wrapper 
+
+     </div><!--wrapper
 </div><!-- end info-->
-    
 
 
-</div> <!--end info--> 
-            
+
+</div> <!--end info-->
+
 
 <?php include('footer.php')?>
--- index.php
+++ index.php
@@ -6,30 +6,30 @@
     <span>Events across the UK</span>
 </div>
 
-<div class="container index"> 
-    
-    <div class="accordion">  
+<div class="container index">
+
+    <div class="accordion">
          <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i> <span class="two-lines">About the DD<div class="caps-change">a</div>T Roundtable events</span></h3>
     <div>
-        <div class="wrapper">   
+        <div class="wrapper">
             <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team.</p>
- 
+
 <p>They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT. We may also theme some of the conversations to address key or topical subjects.</p>
- 
+
 <p>Sessions will be co-hosted by senior managers and take place across our various locations on a quarterly basis.</p>
- 
+
 <p>All sessions are open to everyone across DDaT (subject to business commitments) and <strong>places must be booked through this app</strong>. Tickets are available on a first come first served basis and waiting lists will be in operation. You are not expected to travel specifically to attend a session; further roundtables will be held at each site on a rolling basis so there will be plenty of opportunities to attend throughout the year. Roundtables will not be used as a channel to announce any major updates – if required, such information would be shared in the usual ways with all staff. </p>
- 
+
 <p>Once you have booked a place this will be considered as your commitment to attend. Calendar appointments and reminders will be sent out before the date but <strong>please keep revisiting this app for the latest updates</strong> or changes to the agenda.</p>
         </div><!--wrapper-->
     </div><!-- end venue-->
 
-        
+
          <h3><i class="fa fa-calendar fa-lg" aria-hidden="true"></i> <span>Previous events</span></h3>
     <div>
-        <div class="wrapper">   
+        <div class="wrapper">
             <p>In 2017 we held roundtable events at:</p>
-            
+
             <ul>
                 <li>Liverpool - 25 April</li>
                 <li>London - 27 April</li>
@@ -47,65 +47,53 @@
                 <li>Bedfont Lakes - 29 November</li>
                 <li>Hendon - 30 November</li>
                 <li>Sheffield - 4 December</li>
-            
+
             </ul>
 
               <p>Our 2018 roundtables have included:</p>
-            
+
             <ul>
                 <li>Croydon - 25 January</li>
                 <li>Hendon - 7 February</li>
                 <li>Liverpool - 8 March</li>
                 <li>Southports - 8 March</li>
                 <li>Sheffield - 15 March</li>
-                
+                <li>Bedfont Lakes - 27 March</li>
+
             </ul>
-            
+
         </div><!--wrapper-->
     </div><!-- end venue-->
-        
 
-      <h3 class="ui-accordion-header">
-         <i class="fa fa-globe fa-lg" aria-hidden="true"></i> <span>Bedfont Lakes - 27 March</span></h3>
-     <div>
-        <div class="wrapper">   
-            <p>Our next roundtable event in Bedfont Lakes will be held on 27 March 2018.</p>
-<p>Please email the <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications inbox</a> if there are any specific topics or issues you would like added to the agenda.</p>
-            <div class="button_container_first">
-                <a class="btTxt" href="/bedfont.php">View event</a>
-            </div>
-        </div><!--wrapper-->
-        </div><!-- end hendon--> 
-        
+
               <h3 class="ui-accordion-header">
-         <i class="fa fa-globe fa-lg" aria-hidden="true"></i> <span>Croydon - April</span></h3>
+         <i class="fa fa-globe fa-lg" aria-hidden="true"></i> <span>Croydon - 9 May</span></h3>
      <div>
-        <div class="wrapper">   
-            <p>Our next roundtable event in Croydon will be held on in April 2018.</p>
+        <div class="wrapper">
+            <p>Our next roundtable event in Croydon will be held on on 9th May 2018.</p>
 <p>Please email the <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications inbox</a> if there are any specific topics or issues you would like added to the agenda.</p>
             <div class="button_container_first">
                 <a class="btTxt" href="/croydon.php">View event</a>
             </div>
         </div><!--wrapper-->
-        </div><!-- end hendon--> 
-        
-       </div><!--end accordion--> 
-    
+        </div><!-- end hendon-->
+
+       </div><!--end accordion-->
+
     <div class="coming-soon ui-accordion">
     </div><!-- coming soon-->
-    
-    <div class="accordion">  
-        
+
+    <div class="accordion">
+
     <h3><i class="fa fa-calendar fa-lg" aria-hidden="true"></i> <span>Future events</span></h3>
         <div>
             <div class="wrapper">
                 <p>The Digital, Data and Technology roundtables will take place across our main locations on a quarterly basis. These are the proposed DDaT Roundtable dates for 2018.</p>
-                
+
               <ul>
-                <li>Croydon - 9 May</li>
                 <li>Hendon - 16 May</li>
             </ul>
-                    
+
 <p><strong>Please keep revisiting this tab for the latest forward look information.</strong></p>
             </div> <!-- wrapper-->
         </div><!-- end register-->
@@ -117,9 +105,9 @@
             <p>For the best experience using this site, we recommend you use Google Chrome or the Microsoft Edge browser.</p>
             <div class="footer_button_container">
        <a class="btTxt" href="https://ddat-together-events.herokuapp.com/"><i class="fa fa-angle-double-left fa-lg" aria-hidden="true"></i> Back to the main DDaT events site</a>
-            </div>    
+            </div>
     </footer>
-    
+
 </div>
     </body>
 </html>
