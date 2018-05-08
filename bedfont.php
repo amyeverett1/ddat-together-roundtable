1001f41b101e8d90c1f6a05044b626f04e1de674
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-05-08 16:15:09 +0100

    added bedfont page

--- bedfont.php
+++ bedfont.php
@@ -2,80 +2,76 @@
     <h1>Bedfont Lakes Roundtable</h1>
 </header>
 
-<div class="date">
-    <span>Bedfont Lakes, 6 New Square, Feltham TW14 8HA | 27 March 2018 </span>
-</div>
+<div class="container index">
+<div class="accordion">
 
-<div class="container index"> 
-<div class="accordion">    
-    
-    
-     <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Agenda</span></h3>
-    
+
+     <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Event information</span></h3>
+
 <div>
 <div class="wrapper">
+  <div class="date">
+      <span>Ground floor, Bedfont Lakes, Feltham<br> 22 May 2018 <br> 11:00-12:30 <br> Hosts: Pete Rose, Juliette Layne, Geoff Teale</span>
+  </div>
+    <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team. They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT. Everyone is welcome to attend any DDaT roundtable event (subject to business needs).</p>
 
-    <p>The Digital, Data and Technology roundtables are informal face to face sessions with the DDaT senior management team. They are an opportunity for relaxed ‘get to know you and your work’ conversations with colleagues and senior managers who you might not usually meet in your day to day activities and to help build wider awareness, understanding and collaboration across DDaT.</p>
+            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events. This is your roundtable to find out more about the wider work of DDaT and discuss developments, local issues and concerns. It is also an opportunity to share information about your work, what your team is doing and your successes. You can also raise questions and issues in advance by emailing the DDaT Communications team. Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please click on <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">this link</a>.</p>
 
-            <p>All sessions are open to everyone across DDaT (subject to business commitments). You do not need to book in advance; places will be available on a first come first served basis on the day. You are not expected to travel specifically to attend a session; further roundtables will be held at each site on a rolling basis so there will be plenty of opportunities to attend throughout the year.</p>
-
-            <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events. This is your roundtable to find out more about the wider work of DDaT and discuss developments, local issues and concerns. It is also an opportunity to share information about your work, what your team is doing and your successes.  You can also raise questions and issues in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">submit questions or queries using this link</a>.</p>
+            <p>The event will be hosted by Pete Rose, Juliette Layne and Geoff Teale.</p>
 
             <p>These events are coordinated by <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">DDaT Communications</a>.</p>
               <p>We won't be releasing tickets for this event. If you wish to come along please just turn up.</p>
-            <p><strong>13:30pm Start</strong></p>     
+            <p><strong>13:30pm Start</strong></p>
             <ul>
-                <li>Welcome and open – Juliette Layne</li>
-                <li>Introductions – All</li>
-                <li>Business updates and key developments – Juliette Layne</li>
-                <li>Hendon updates including successes – All</li>
-                <li>Questions and discussion topics submitted in advance by attendees – All</li>
+                <li>Welcome and introductions – Juliette</li>
+                <li>Business updates and key developments – Pete, Geoff, Juliette</li>
+                <li>Group discussion re topics/questions submitted in advance and/or on the day – All</li>
              </ul>
-            <p><strong>14:30pm Close</strong></p>
-            <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p> 
-    
+            <p><strong>14:45pm Close</strong></p>
+            <p>Please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HO DDaT Communications</a> if you have any queries.</p>
+
     </div> <!-- wrapper-->
-    
+
   </div><!-- end agenda-->
-    
-    
-    
+
+
+
   <!--  <h3><i class="fa fa-user-plus fa-lg" aria-hidden="true"></i> <span>Register to attend</span></h3>
   <div>
 
 <div class="wrapper white">
-    
-    
+
+
    <div style="width:100%; text-align:left;"><iframe src="//eventbrite.co.uk/tickets-external?eid=39548059310&ref=etckt" frameborder="0" height="275" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe></div>
-    
+
     <p>If you experience any issues booking your ticket please contact <a href="mailto:HODDaTcommunications@homeoffice.gsi.gov.uk">HODDaTcommunications@homeoffice.gsi.gov.uk</a></p>
 
-    
-    
+
+
      </div><!--wrapper
 
   </div><!-- end register-->
 
-   
-    
-    
-    
+
+
+
+
  <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Venue</span></h3>
   <div>
-   
-    
-   <div class="wrapper">    
+
+
+   <div class="wrapper">
     <div class="box white">
-    
+
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.5708046283244!2d-0.4487108842322037!3d51.447675579625304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48767394c8ed4d99%3A0xcc4c8a65a1d559ae!2s6+New+Square%2C+Feltham+TW14+8HA!5e0!3m2!1sen!2suk!4v1493309021256" frameborder="0" style="border:0" allowfullscreen></iframe>
-            
-            </div><!--boxwhite--> 
-</div><!--wrapper--> 
-      
-      
+
+            </div><!--boxwhite-->
+</div><!--wrapper-->
+
+
 <div class="wrapper">
      <p>Don’t forget to bring your Home Office pass. You must show it when you arrive. If you don’t bring it, you may not be able to gain access to the building.</p>
-    
+
     <h3>Address</h3>
    <p>
        Bedfont Lakes<br>
@@ -83,35 +79,35 @@
     Feltham<br>
        TW14 8HA</p>
 
-     </div><!--wrapper--> 
+     </div><!--wrapper-->
 
   </div><!-- end venue-->
 
- 
-   
 
 
-  
-    
-    
+
+
+
+
+
 <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i>  <span>Submit your questions</span></h3>
   <div>
 
 <div class="wrapper">
     <p>There is no fixed agenda or formal presentation and you can raise any topic you wish at these events.</p>
-    
+
 <p>We are keen for attendees to raise questions, issues and suggested discussion topics in advance by emailing the DDaT Communications team.  Alternatively, if you’d prefer to submit questions and/or flag issues anonymously, please click on the following link: <a href="http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/">http://www.homeofficesurveys.homeoffice.gov.uk/s/RoundtableQuestions/</a> .</p>
-  
-     </div><!--wrapper--> 
+
+     </div><!--wrapper-->
 
 
   </div><!-- end info-->
 
 
-      <h3><i class="fa fa-comment fa-lg" aria-hidden="true"></i>  <span>Let’s Talk About</span></h3>
+      <!-- <h3><i class="fa fa-comment fa-lg" aria-hidden="true"></i>  <span>Let’s Talk About</span></h3>
   <div>
 
-<!-- <div class="wrapper">
+ <div class="wrapper">
     <img src="/safe.jpg" alt="safe to talk about logo"/>
    <strong><h3 class="space-space">Learning and Development</h3></strong>
     <h3>Background</h3>
@@ -119,10 +115,10 @@
 </p>
 
 <h3>This month</h3>
-    
-    
+
+
 <p>The Home Office recognises the importance of learning and development. Senior leaders in the Department wish to promote a culture where learning is valued and recognised as one of the most important tools for our development, both as individuals and as an organisation. With this in mind, we are pleased to introduce you to our new journey, 'learning to learn'.</p>
-    
+
 <strong>What does this mean for learners?</strong>
 <p>Many of us in the Home Office regularly undertake more than the annual five learning and development days that are encouraged by the Civil Service. However, much of our learning will focus on mandatory training requirements or upskilling activities, such as the workshops on the new performance management system. While these are important learning activities, there is a variety of other ways you can learn and develop individually and as a team.</p>
 
@@ -131,35 +127,35 @@
 
 <strong>What is social learning?</strong>
     <img src="/img/img1a.png" alt="image shows a sliding scale of learning, from the event, to the momentum, and then the intergration into the workplace">
-    
+
 <p>Social learning is not often recognised as “learning” as it is something we do as part of our everyday roles, for example, having discussions with colleagues, reading interesting articles, or catching up on LinkedIn. We learn from each other via observation, imitation and modelling. The challenge for us in the workplace is to make social learning more intentional and effective.</p>
 
     <img src="/img/img2a.jpg" alt="image shows the 70, 20, 10 learning model. 70 percent on the job experience, 20 percent mentoring and 10 percent classroom courses">
-    
+
     <p>Social learning forms part of the ‘learning from experience’ (70%) and ‘learning from others’ (20%) sections of the 70:20:10 Model for Learning and Development, which is a commonly used formula to describe the optimal sources of professional learning. For information about what this means in practice, we encourage you to watch the short video below.</p>
-    
-    
+
+
 <h3>70-20-10 | Social Learning | Charles Jennings</h3>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/t6WX11iqmg0?rel=0" frameborder="0" allowfullscreen></iframe>
-    
+
 <h3>Suggested questions</h3>
     <p>In your teams, we would like you to consider the following questions:</p>
     <ul>
-    
+
     <li>How do you and your team plan to build learning into your everyday work and what support do you need to be able to do this?</li>
  <li>What does being a ‘professional’ mean to you? If applicable, how connected are you with your professions community?</li>
  <li>Where do you go to access learning at work to meet your corporate or development objectives?</li>
  <li>Do you think that the learning you undertake is recognised by managers in the workplace?</li>
     </ul>
-    
-
-    
-     </div><!--wrapper 
-</div><!-- end info-->
-    
 
 
-</div> <!--end info--> 
-            
+
+     </div>
+</div> end info-->
+
+
+
+</div> <!--end info-->
+
 
 <?php include('footer.php')?>
